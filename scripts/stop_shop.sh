b1337566bb7115ea494f54c6a4ef37174fa36327
Unf0rgiv3n <jakub.kwia2@wp.pl>
2021-11-01 21:18:36 +0100

    git ignore chagnes, stop_shop.sh imrpovement

--- scripts/stop_shop.sh
+++ scripts/stop_shop.sh
@@ -4,4 +4,5 @@ echo 'Removing old dump file'
 pushd ../mysql-db && rm -f * && popd
 echo 'Making db dump file'
 bash db-backup.sh
+git update-index --skip-worktree ../mysql-db/databasedump.sql
 docker-compose stop && echo y | docker-compose rm
